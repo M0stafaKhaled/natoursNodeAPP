bfd750196dc1d171eca1da44f821a4b4789e914a
unknown <mkostafa79@gmail.com>
2020-03-20 07:32:57 +0200

    refactor some methods

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const app = express();
 //  server port namber
 app.use(express.json());
 const port = 300;
-
 const tours = JSON.parse(file.readFileSync('dev-data/data/tours-simple.json'));
 //  base route
 app.get('/', (req, res) => {
@@ -13,29 +12,30 @@ app.get('/', (req, res) => {
     app: 'natours'
   });
 });
-// api route  method read data from json file  tours
-app.get('/api/v1/tours', (req, res) => {
+const getTours = (req, res) => {
   res.status(200).json({
     status: 'scucess',
     result: tours.length,
     data: { tours }
   });
-});
-
-// read single data from tours
-app.get('/api/v1/tours/:id', (req, res) => {
+};
+const getTour = (req, res) => {
   const id = req.params.id * 1;
 
   const tour = tours.find(el => el.id === id);
+  if (!tour) {
+    return res.status(404).json({
+      status: 'field',
+      massge: 'invalid id'
+    });
+  }
+
   res.status(200).json({
     status: 'scucess',
     data: { tour }
   });
-});
-
-//  make post request to  add  new tour data to json file
-
-app.post('/api/v1/tours', (req, res) => {
+};
+const createTour = (req, res) => {
   const newid = tours[tours.length - 1].id + 1;
   const newtour = Object.assign({ id: newid }, req.body);
   tours.push(newtour);
@@ -46,8 +46,17 @@ app.post('/api/v1/tours', (req, res) => {
       res.status(201).json({ status: 'scucess', data: { newtour } });
     }
   );
-});
-//
+};
+// api route  method read data from json file  tours
+app.get('/api/v1/tours', getTours);
+
+// read single data from tours
+app.get('/api/v1/tours/:id', getTour);
+
+//  make post request to  add  new tour data to json file
+
+app.post('/api/v1/tours', createTour);
+
 app.listen(port, () => {
   console.log('hello from the server');
 });
