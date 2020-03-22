212f6a38a9abea1e6a094d181927a94261e935a9
unknown <mkostafa79@gmail.com>
2020-03-22 12:51:01 +0200

    add middleWare to static folder

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const userRoute = require('./routes/userRoute');
 const app = express();
 //  server port namber
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 //  base route
 app.use('/api/v1/tours', tourRoute);
